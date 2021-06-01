55e380b38de4a415068eeeb33113fbea485b28a8
Parsa <mehdipourparsa@gmail.com>
2021-06-01 12:54:59 +0430

    Add project files.

---
+++ Singleton Design/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Singleton_Design
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
