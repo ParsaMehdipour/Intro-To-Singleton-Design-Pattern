1822011a1dc0035a4f2c8e195dcd6f5b46244ace
Parsa <mehdipourparsa@gmail.com>
2021-06-01 13:12:53 +0430

    refactor : use of singleton pattern

--- Singleton Design/Program.cs
+++ Singleton Design/Program.cs
@@ -4,9 +4,26 @@ namespace Singleton_Design
 {
     public class Program
     {
+        static TableSevers Host1Server = TableSevers.GetTableServers();
+        static TableSevers Host2Server = TableSevers.GetTableServers();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 5; i++)
+            {
+                Host1GetsWaiters();
+                Host2GetsWaiters();
+            }
+        }
+
+        public static void Host1GetsWaiters()
+        {
+            Console.WriteLine($"The Next Waiter Is : {Host1Server.GetNextWaiter()}");
+        }    
+
+        public static void Host2GetsWaiters()
+        {
+            Console.WriteLine($"The Next Waiter Is : {Host1Server.GetNextWaiter()}");
         }
     }
 }
