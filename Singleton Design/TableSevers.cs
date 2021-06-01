2ed9e0ca8a00dee0a3b5b5b332c0e1541a0f839b
Parsa <mehdipourparsa@gmail.com>
2021-06-01 13:13:11 +0430

    feat : add table servers class

---
+++ Singleton Design/TableSevers.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Singleton_Design
+{
+    public class TableSevers
+    {
+        private static readonly TableSevers _severs = new TableSevers();
+
+        private List<string> Waiters = new List<string>();
+        private int _nextServer = 0;
+
+        public TableSevers()
+        {
+            Waiters.Add("Parsa");
+            Waiters.Add("Mark");
+            Waiters.Add("Bob");
+            Waiters.Add("Sue");
+        }
+
+        public static TableSevers GetTableServers()
+        {
+            return _severs;
+        }
+
+        public string GetNextWaiter()
+        {
+            var output = Waiters[_nextServer];
+
+            _nextServer += 1;
+
+            if (_nextServer >= Waiters.Count)
+                _nextServer = 0;
+
+            return output;
+        }
+    }
+}
